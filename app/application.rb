b5937e30e9d24fcd7fe55d814581e4d32715fd56
Andrew Davisson <akdavisson4@gmail.com>
2019-07-02 16:09:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.each
+      item = @@items.find {|i| i.name == item_name}
       if @@items.include?(item)
         resp.write "#{item.price}"
       else
