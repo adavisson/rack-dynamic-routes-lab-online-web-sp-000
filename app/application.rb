8f554281d181ceb84fcca40b9db8911896635d3d
Andrew Davisson <akdavisson4@gmail.com>
2019-07-02 16:05:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,11 @@ class Application
     if req.path == "/items/"
       item = req.path.split("/items/").last
       if @@item.include?(item)
-        resp.write .
+        resp.write "#{item.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
       resp.write "Route not found"
       resp.status = 404
